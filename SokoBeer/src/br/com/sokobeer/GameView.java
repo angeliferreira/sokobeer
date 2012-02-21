8429396a49fb912868021219c276601e87c053ce
angeliferreira <angeliferreira@8a8344bb-a51a-48bd-ae6a-0157cc885ee1>
2012-02-21 23:41:52 +0000

    * Printing movements using SurfaceView and calling onDraw();

--- SokoBeer/src/br/com/sokobeer/GameView.java
+++ SokoBeer/src/br/com/sokobeer/GameView.java
@@ -7,14 +7,25 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 
-public class GameView extends View {
+public class GameView extends SurfaceView {
+	
+	private SurfaceHolder holder;
 	private Sokobao2000 sokobao;
+	private float oldX;
+	private float newX;
+	private float oldY;
+	private float newY;
 	 
     public GameView(Context context) {
           super(context);
           this.sokobao = new Sokobao2000(); 
+          this.setOnTouchListener(listener);
+          setHolder(getHolder());
     }
     
     @Override
@@ -22,11 +33,10 @@ public class GameView extends View {
         canvas.drawColor(Color.BLACK);
         
         Bitmap[][] bitmapScreen = getBitmapScreen();
-        int i = 0;
 		int j = 20;
         
         for (int x = 0; x < 10; x++) {
-        	i = 20;
+        	int i = 20;
 			for (int y = 0; y < 10; y++) {
 				canvas.drawBitmap(bitmapScreen[y][x], i, j, null);
 				i += 20;
@@ -56,5 +66,59 @@ public class GameView extends View {
 		if (singleName == 'X') return BitmapFactory.decodeResource(getResources(), R.drawable.target);
 		return BitmapFactory.decodeResource(getResources(), R.drawable.empty);
 	}
+	
+	public void setHolder(SurfaceHolder holder) {
+		this.holder = holder;
+	}
 
+	private void updateOnDrawCanvas(SurfaceHolder holder) {
+		Canvas canvas = holder.lockCanvas(null);
+		onDraw(canvas);
+		getHolder().unlockCanvasAndPost(canvas);
+	}
+
+	private OnTouchListener listener = new OnTouchListener() {
+
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+
+			switch (event.getAction()) {
+				case (MotionEvent.ACTION_DOWN):
+					oldX = event.getX();
+					oldY = event.getY();
+					break;
+				case (MotionEvent.ACTION_UP):
+					newX = event.getX();
+					newY = event.getY();
+					float dx = newX - oldX;
+					float dy = newY - oldY;
+	
+					if (dx == 0 && dy == 0) return false;
+					
+					if (Math.abs(dx) > Math.abs(dy)) {
+						if (dx > 0) {
+							//Log.i("lemao", "RIGHT - "+dx);
+							sokobao.moveHeroEast();
+						} else {
+							//Log.i("lemao", "LEFT - "+dx);
+							sokobao.moveHeroWest();
+						}
+					} else {
+						if (dy > 0) {
+							//Log.i("lemao", "DOWN - "+dy);
+							sokobao.moveHeroSouth();						
+						} else {
+							//Log.i("lemao", "UP - "+dy); 
+							sokobao.moveHeroNorth();
+						}
+					}
+					
+					updateOnDrawCanvas(holder);
+			}
+			return true;
+		}
+	};
+	
+	
+	
 }
